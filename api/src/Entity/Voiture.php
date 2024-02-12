6d00f04708fea6a7962d24cf99e31b6fdfef5038
Arkalithe <arkalithe@gmail.com>
2024-02-12 13:53:26 +0100

    creation entity voiture

---
+++ api/src/Entity/Voiture.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\VoitureRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VoitureRepository::class)]
+#[ApiResource]
+class Voiture
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $prix = null;
+
+    #[ORM\Column]
+    private ?int $year = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $path = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $caracteristique = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $equipement = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrix(): ?int
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(int $prix): static
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    public function setYear(int $year): static
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    public function setPath(?string $path): static
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    public function getCaracteristique(): ?string
+    {
+        return $this->caracteristique;
+    }
+
+    public function setCaracteristique(string $caracteristique): static
+    {
+        $this->caracteristique = $caracteristique;
+
+        return $this;
+    }
+
+    public function getEquipement(): ?string
+    {
+        return $this->equipement;
+    }
+
+    public function setEquipement(string $equipement): static
+    {
+        $this->equipement = $equipement;
+
+        return $this;
+    }
+}
